465033b842d5b94f6b4dc8adac2f88d1c8f7ed97
Oliver Breitwieser <oliver@breitwieser.eu>
2020-09-27 17:38:55 +0200

    Add script to make new release

---
+++ tools/make_release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Use `cargo release` to publish a new version and update documentation. 
+
+set -euo pipefail
+
+toplevel="$(git rev-parse --show-toplevel)"
+
+cargo release "${@}"
+"${toplevel}/tools/update-docs-latest-release.sh"
