089627f292db1fb27fe56b7e25588528edb37f69
Oliver Breitwieser <oliver@breitwieser.eu>
2020-10-03 15:24:19 +0200

    ci: Allow for persistent local setup
    Store path to test roof folder in container and use it if container is
    already running.

--- test-utils/setup.sh
+++ test-utils/setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+shopt -s lastpipe
 
 sourcedir="$(dirname "$(readlink -m "${BASH_SOURCE[0]}")")"
+
 
 ####################
 # Helper functions #
@@ -23,68 +25,84 @@ ensure_folder() {
     fi
 }
 
-##########################
-# Setup test root folder #
-##########################
 
-testroot="$(mktemp -d)"
-ensure_folder "${testroot}"
+#################################
+# Check if container is running #
+#################################
 
-###############
-# Copy config #
-###############
+if docker exec asfa-ci cat /config/test_root 2>/dev/null | read -r test_root; then
+    container_set_up=1
+else
+    container_set_up=0
+    test_root="$(mktemp -d)"
+fi
 
-ensure_folder "${testroot}"
-export_var ASFA_TEST_ROOT "${testroot}"
 
-folder_config="${testroot}/config"
+##########
+# Config #
+##########
+
+ensure_folder "${test_root}"
+export_var ASFA_TEST_ROOT "${test_root}"
+
+folder_config="${test_root}/config"
 ensure_folder "${folder_config}"
 
-cp "${sourcedir}/ci-config/raw.yaml" "${folder_config}/config.yaml"
+if (( container_set_up == 0 )); then
+    cp "${sourcedir}/ci-config/raw.yaml" "${folder_config}/config.yaml"
+fi
 export_var ASFA_CONFIG "${folder_config}"
 
-#########################
-# Restore test-ssh keys #
-#########################
 
-TEST_SSH_PRIVKEY_FILE="${ASFA_CONFIG}/test.key"
+########################
+# Create test-ssh keys #
+########################
 
-echo -n "${TEST_SSH_PRIVKEY_B64}" | openssl base64 -A -d > "${TEST_SSH_PRIVKEY_FILE}"
-sed -i "s:TEST_SSH_PRIVKEY_FILE:${TEST_SSH_PRIVKEY_FILE}:" "${ASFA_CONFIG}/config.yaml"
+TEST_SSH_PRIVKEY_FILE="${ASFA_CONFIG}/test_key.pem"
+TEST_SSH_PUBKEY_FILE="${TEST_SSH_PRIVKEY_FILE}.pub"
 
-TEST_SSH_PUBKEY_FILE="${testroot}/test.pub"
-echo "${TEST_SSH_PUBKEY}" > "${TEST_SSH_PUBKEY_FILE}"
+if (( container_set_up == 0 )); then
+    ssh-keygen -t ed25519 -f "${TEST_SSH_PRIVKEY_FILE}" -m PEM -P "" >&2
+    sed -i "s:TEST_SSH_PRIVKEY_FILE:${TEST_SSH_PRIVKEY_FILE}:" \
+        "${ASFA_CONFIG}/config.yaml"
+fi
+
 
 #################
 # Set up docker #
 #################
 
-export_var ASFA_FOLDER_UPLOAD "${testroot}/uploads"
+export_var ASFA_FOLDER_UPLOAD "${test_root}/uploads"
 ensure_folder "${ASFA_FOLDER_UPLOAD}"
 
-folder_docker_config="${testroot}/docker-cfg"
+folder_docker_config="${test_root}/docker-cfg"
 ensure_folder "${folder_docker_config}"
 
-if (( $(docker container ls -q | wc -l) == 0 )); then
+if (( container_set_up == 0 )); then
+    echo "${test_root}" > "${folder_docker_config}/test_root"
+
     docker build -t asfa-ci-image - >&2 <<EOF
 FROM linuxserver/openssh-server
 # needed for scp-functionality
 RUN apk add --no-cache openssh-client
 EOF
-    docker create \
-      --name=asfa-ci \
-      -e "PUID=$(id -u)" \
-      -e "PGID=$(id -g)" \
-      -e TZ=Europe/London \
-      -e PUBLIC_KEY_FILE=test.pub \
-      -e PASSWORD_ACCESS=true \
-      -e USER_PASSWORD=foobar \
-      -e USER_NAME=asfa-ci-user \
-      -p 2222:2222 \
+    docker create                                         \
+      --name=asfa-ci                                      \
+      -e "PUID=$(id -u)"                                  \
+      -e "PGID=$(id -g)"                                  \
+      -e TZ=Europe/London                                 \
+      -e "PUBLIC_KEY_FILE=${TEST_SSH_PUBKEY_FILE}"        \
+      -e PASSWORD_ACCESS=true                             \
+      -e USER_PASSWORD=foobar                             \
+      -e USER_NAME=asfa-ci-user                           \
+      -p 2222:2222                                        \
       -v "${ASFA_FOLDER_UPLOAD}:/var/www/default/uploads" \
-      -v "${folder_docker_config}":/config \
-      --restart unless-stopped \
+      -v "${folder_docker_config}":/config                \
+      --restart unless-stopped                            \
       asfa-ci-image >&2
 
     docker start asfa-ci >&2
+
+    ensure_folder "${folder_docker_config}/.ssh"
+    cat "${TEST_SSH_PUBKEY_FILE}" >> "${folder_docker_config}/.ssh/authorized_keys"
 fi
