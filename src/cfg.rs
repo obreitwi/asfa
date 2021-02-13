a5ec102450d174ed67ad48833a0fd5a4773762dd
Oliver Breitwieser <oliver@breitwieser.eu>
2021-02-13 12:51:54 +0100

    Remove unneeded lifetime specifier

--- src/cfg.rs
+++ src/cfg.rs
@@ -348,7 +348,7 @@ impl Host {
     }
 
     /// Get hostname as configured or a supplied default or (finally) alias name of the host.
-    pub fn get_hostname_def<'a>(&'a self, default: Option<String>) -> String {
+    pub fn get_hostname_def(&self, default: Option<String>) -> String {
         self.hostname
             .clone()
             .or(default)
