1274804fb5d87b092d498ba8fa5bc617f35336f4
Oliver Breitwieser <oliver@breitwieser.eu>
2021-08-03 14:46:31 +0200

    Improve binary generation script
    Added the ability to release on github.

--- tools/make-binary-release.sh
+++ tools/make-binary-release.sh
@@ -26,6 +26,7 @@ check_existing awk
 check_existing cargo
 check_existing find
 check_existing help2man
+check_existing gh
 
 tmp_build="$(mktemp -d)"
 
@@ -60,11 +61,31 @@ help2man "${path_bin}" > "${folder_man}/asfa.1"
     | while read -r cmd; do
     help2man "'${path_bin}' $cmd" > "${folder_man}/asfa-${cmd}.1"
 done
+find "${folder_man}" -type f -print0 | xargs -0 gzip
 
 cp -a "${toplevel}/example-config" "${folder_release}/example-config"
+find "${folder_release}/example-config" -type f -print0 | xargs -0 chmod 644
+
+include_file() {
+    install -Dm644 "${toplevel}/$1" "${folder_release}"
+}
+
+include_file LICENSE-MIT
+include_file LICENSE-APACHE
+include_file README.md
+include_file CHANGELOG.md
 
 cd "${folder_release}/.."
 
 echo "Creating: ${release_archive}"
 echo
 tar cfvz "${release_archive}" "${release}"
+
+cd "${toplevel}"
+echo "Create github release for ${release_archive}? (y/n)"
+read -r line
+if [ "${line}" == y ]; then
+    gh release create "v${version}" "${release_archive}" -F <(${toplevel}/tools/changelog_version.sh "${version}")
+else
+    echo "Aborting.." >&2
+fi
